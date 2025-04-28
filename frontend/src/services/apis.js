cf20c62d0177c0775ab340c3487305739349edea
SandyN <ssnnwc@gmail.com>
2025-04-28 15:15:56 +0530

    updated code file

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3000/api/v1"
+const BASE_URL = process.env.NODE_ENV === 'production' ? `http://${process.env.BACKEND_IMAGE}:3000/api/v1` : "http://localhost:3000/api/v1";
+
 
 export const instructorEndpoints = {
     SIGNUP_API: BASE_URL + "/instructor/signUp",
